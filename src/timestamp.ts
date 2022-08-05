9849b9a3a571fe739816089fad694016d84d3eba
Gilles Barnier <gilles.barnier@gmail.com>
2022-08-04 20:22:21 -0600

    fix typo

---
+++ src/timestamp.ts
@@ -0,0 +1,47 @@
+import * as PromClient from "prom-client"
+import { logger } from '../logger';
+import { Exporter } from './IExporter';
+import { ApiPromise } from "@polkadot/api";
+import { Header } from "@polkadot/types/interfaces";
+import { Timestamp } from '../workers/timestampWorker'
+import { TIMESTAMP_WORKER_PATH } from '../workers/workersPaths'
+
+class TimestampExporter extends Timestamp implements Exporter {
+    palletIdentifier: any;
+    registry: PromClient.Registry;
+
+    constructor(registry: PromClient.Registry) {
+        super(TIMESTAMP_WORKER_PATH, registry, true);
+        this.registry = registry;
+        this.palletIdentifier = "timestamp";
+
+    }
+
+    async init(api: ApiPromise, chainName: string, startingBlockTime: Date, endingBlockTime: Date) {
+
+        await this.clean(api, chainName.toString(), startingBlockTime, endingBlockTime);
+
+    }
+
+    async perBlock(api: ApiPromise, header: Header, chainName: string): Promise<void> {
+
+        const blockNumber = parseInt(header.number.toString());
+        const result = await this.doWork(this, api, blockNumber, chainName);
+
+    }
+
+    async launchWorkers(threadsNumber: number, startingBlock: number, endingBlock: number, chain: string) {
+        super.launchWorkers(threadsNumber, startingBlock, endingBlock, chain)
+
+    }
+
+    async perDay(api: ApiPromise, chainName: string) { }
+
+    async perHour(api: ApiPromise, chainName: string) { }
+
+
+}
+
+export { TimestampExporter };
+
+
